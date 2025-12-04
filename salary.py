38f7d25f2b0d9c098332fec33a2c28e64c3b6dcc
Swapna-Karlawad <swapnakarlawad@gmail.com>
2025-12-04 17:34:35 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -4,10 +4,8 @@ if len(sys.argv) == 3:
     salary = float(sys.argv[1])
     bonus_percentage = float(sys.argv[2])
 else:
-    print("No input given - using default values.")
     salary = 20000
     bonus_percentage = 10
-
 bonus_amount = salary * (bonus_percentage / 100)
 final_salary = salary + bonus_amount
 
