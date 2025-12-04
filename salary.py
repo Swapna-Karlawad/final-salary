4282d6c228e67ae46c72d3966f2ce40652030cb1
swapna-karlawad <swapnakarlawad@gmail.com>
2025-12-04 16:52:00 +0530

    save

--- salary.py
+++ salary.py
@@ -1,13 +1,16 @@
 import sys
 
 if len(sys.argv) != 3:
-    print("Usage: python script.py <salary> <bonus_percentage>")
-    sys.exit()
-
-salary = float(sys.argv[1])
-bonus_percentage = float(sys.argv[2])
+    print("No input given - using default values.")
+    salary = 20000
+    bonus_percentage = 10
+else:
+    salary = float(sys.argv[1])
+    bonus_percentage = float(sys.argv[2])
 
 bonus_amount = salary * (bonus_percentage / 100)
 final_salary = salary + bonus_amount
 
-print("Final salary:", final_salary)
+print("Salary:", salary)
+print("Bonus Percentage:", bonus_percentage)
+print("Final Salary:", final_salary)
